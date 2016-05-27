49f862603f3098e4a909479dabce6668be5f5e99
Lance Barney <lbarney@littleappletech.com>
2016-05-26 21:36:54 -0600

    update

--- toy_problem1.js
+++ toy_problem1.js
@@ -14,10 +14,6 @@ return newString;
 
 
 
-
-
-
-
 //TOY PROBLEM 2
 //Write a function that randomly creates and returns a hex color code. Remember, hex color codes are comprised of 6 characters led by a hash. Each character could be any number 0-9 or any letter A-F.
 var hexColorGen = function(){
@@ -26,4 +22,38 @@ var color = num.toString(16);
   return ('#' + color);
 };
 console.log(hexColorGen());
+
+// Me and Colter's Solution
+var hexColorGenerator = function(){
+    var color = '';
+    var colorArr = [];
+    var arr = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'];
+    var randomIndex = 0;
+    for(var i = 0; i < 6; i++ ){
+      randomIndex = Math.round(Math.random() * 15);
+      colorArr.push(arr[randomIndex]);
+    }
+      color = colorArr.join('');
+      return '#' + color;
+  };
+  console.log(hexColorGenerator());
+
+  //another way to generate number between 0 an 15
+  // var colorGenerator=function(){
+  // var randomIndex;
+  // var i=0;
+  // while(i<10){
+  //     randomIndex=Math.round(Math.random()*1000);
+  //     console.log(randomIndex);
+  //     randomIndex= randomIndex%16;
+  //     console.log(randomIndex+'\n');
+  //     i++;
+  // }
+  // };
+  // colorGenerator();
+
+
+
+
+
 
